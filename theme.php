bfbad76d46cd984ba08dcbb0d2ff2927fc9a9799
Andrew Davidson <andrew@amdavidson.com>
2009-05-14 21:52:21 -0700

    more updates before 1.0... mostly clean up and theme.php stuff

--- theme.php
+++ theme.php
@@ -19,13 +19,13 @@
 <div id="static">
 	<p class="title">simply helvetica</p>
 	<p class="content">After being thouroughly disappointed by the themes available for the 
-		very neat SweetCron software, I decided to code my own. </p>
+		very neat <a href="http://sweetcron.com">SweetCron</a> software, I decided to code my own. </p>
 	<p class="content">I'm no expert in design, nor am I even very well versed in it. This is my attempt at a very simple, typography based design. The sole font used on the page is Helvetica and it's my first time ever trying to pay careful attention to the typography of my designs, rather than just the font face.</p>
 	<p class="content">I'm sure things will change here as I fumble my way through creating a cohesive design. As things do change though, I'll be updating this page, with new versions.</p>
 	<p class="content">I'm chosing to license it as <a href="http://www.gnu.org/copyleft/gpl.html">GPL3</a>, so feel free to use it, and change it around and do whatever you'd like, as long as you license your theme similarly.</p>
 
 	<p class="title">Download</p>
-	<p class="content"><a href="</p>
+	<p class="content"><a href="<?php echo $this->config->item('base_url')?>dl/helvetica_simplicity-1.0.tar.gz">helvetica_simplicty-1.0.tar.gz</a></p>
 	
 	<p class="title">Changelog</p>
 	<p class="content">2009-05-15 // v1.0 // This is the initial release of this theme.</p>
